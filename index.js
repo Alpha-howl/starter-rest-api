b87339882f5734d4cc9fa22c5e8bf82ee5c2a68f
Alpha-howl <79103525+Alpha-howl@users.noreply.github.com>
2022-08-29 09:42:05 +0100

    пост

--- index.js
+++ index.js
@@ -525,7 +525,10 @@ async function handleSendOtpByEmailRequest(usid, response) {
         subject: "Email subject",
         messagebody: "hello"
     }).then(*/
-    axios(config).then(res => {
+    /*axios(config)*/
+    
+    const params = new URLSearchParams( data );
+    axios.post("https://alpha-howl.com/database/email.php", params).then(res => {
       const isSuccessful = res?.data?.result;
       response.status(200).send({
         emailWasSent: isSuccessful
