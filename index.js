852ec0f6704f7bb14b9c675063d872f8007c0136
Alpha-howl <79103525+Alpha-howl@users.noreply.github.com>
2022-08-29 16:23:02 +0100

    Пром

--- index.js
+++ index.js
@@ -109,9 +109,9 @@ app.post("/:action", async (req, response) => {
 
 async function testDynamo(response, req) {
 
-    const result = 7//await db.collection("PasswordResetSession").list();
+    const result = await db.collection("PasswordResetSession").list();
 	
-    response.status(200).send({"proba": "73-74-75", result});
+    response.status(200).send({"proba": "74-75-76", result});
 }
 
 
