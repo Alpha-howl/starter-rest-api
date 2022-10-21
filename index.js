50dd7956e833817ce7581525d484a465865ac44c
Alpha-howl <79103525+Alpha-howl@users.noreply.github.com>
2022-10-21 19:03:54 +0100

    п

--- index.js
+++ index.js
@@ -1464,8 +1464,8 @@ async function handleReadyToPlayRequest(roomId, jwt, response) {
         return;
     }
 
-    if(! (roomData?.joinedPlayers?.contains(username))) {
-        // if the user has tampered with the request payload
+    if(! (roomData?.joinedPlayers?.includes(username))) {
+        // if the user has tampered with the request payload (ie changed the roomid of the sent req)
         // reject their request
         response.status(200).send({
             success: false, message: "unknown-error", roomData: {gotten: await (db.collection("Room").get(roomId.toString()))}
