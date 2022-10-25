696a4d164d8fcdd23b2e5e853fab87657040d1cf
Alpha-howl <79103525+Alpha-howl@users.noreply.github.com>
2022-10-25 18:21:17 +0100

    й

--- index.js
+++ index.js
@@ -409,11 +409,7 @@ async function pubnubOpen(response) {
 
     setTimeout(() => {
         response.status(200).send("closing");
-    }, 20000);
-
-    setTimeout(() => {
-        console.log("Closed");
-    }, 4 * 60 * 1000);
+    }, 29000);
 }
 async function mazeProba(response) {
     const newMaze = randomDfs(11, 11);
